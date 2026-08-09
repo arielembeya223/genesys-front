8406d7de54d2a2cfc8338c0e4dbb4dbee494d6f1
ariel <arielshongoordi@gmail.com>
2026-08-09 12:55:10 -0400

    'deuxieme'

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,11 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+    base: process.env.FIGMA_PUBLIC_URL
+      ? `${process.env.FIGMA_PUBLIC_URL}/`
+      : mode === 'production'
+        ? '/genesys-front/'
+        : '/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
@@ -353,4 +357,4 @@ function figmaMakeKitPlugin(options: { storiesGlob: string | string[] }): Plugin
       })
     },
   }
-}
+}s
